a53e7d82d38368cc0345e77110fb1512b5dc207a
pagalpanda <111.amit.p@gmail.com>
2015-09-30 01:31:54 +0530

    Added funtionality for OTP-verification timer-navigation to Manual-OTP ver. screen

--- app/src/main/java/common/barter/com/barterapp/ManageUser.java
+++ app/src/main/java/common/barter/com/barterapp/ManageUser.java
@@ -6,6 +6,7 @@ package common.barter.com.barterapp;
 import android.app.Activity;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.DialogFragment;
@@ -90,22 +91,22 @@ public class ManageUser extends Fragment {
                 flash("Button clicked");
                 LoginDetails.getInstance().setIsverifying(true);
                 //setPopUp(inflater);
-                setDialogFragment();
+                receiveWebOTP();
 
             }
         });
 
-        btverify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //etOTP.setVisibility(View.VISIBLE);
-                flash("Button clicked");
-                LoginDetails.getInstance().setIsverifying(true);
-                //setPopUp(inflater);
-                setDialogFragment();
-
-            }
-        });
+//        btverify.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //etOTP.setVisibility(View.VISIBLE);
+//                flash("Button clicked");
+//                LoginDetails.getInstance().setIsverifying(true);
+//                //setPopUp(inflater);
+//                setDialogFragment();
+//
+//            }
+//        });
 
         btchangePwd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -172,24 +173,7 @@ public class ManageUser extends Fragment {
     }
 
 
-    public void setDialogFragment()
-    {
-        Fragment fragment = new OTPFragment();
 
-        if (fragment != null) {
-
-//            fragmentManager.beginTransaction()
-//                    .replace(R.id.frame_container, fragment).addToBackStack("Manage User").commit();
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.frame_container, fragment).commit();
-
-        } else {
-            // error in creating fragment
-            Log.e("MainActivity", "Error in creating Manage user fragment");
-        }
-
-
-    }
 
     public  void flash(String message){
         Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
@@ -222,6 +206,194 @@ public class ManageUser extends Fragment {
                     }
                     else {
                         LoginDetails.getInstance().setIsverifying(false);
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        as.execute();
+
+    }
+
+    public void setDialogFragment()
+    {
+        Fragment fragment = new OTPFragment();
+
+        if (fragment != null) {
+
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.frame_container, fragment).addToBackStack("Manage User").commit();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.frame_container, fragment).commit();
+
+        } else {
+            // error in creating fragment
+            Log.e("MainActivity", "Error in creating Manage user fragment");
+        }
+
+
+    }
+
+    public void createProgressBarProperties(){
+        progress=new ProgressDialog(getContext());
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setCancelable(false);
+        progress.setCanceledOnTouchOutside(false);
+
+        progress.setIndeterminate(true);
+        progress.setProgressNumberFormat(null);
+        progress.setProgressPercentFormat(null);
+        progress.setTitle("OTP Verification");
+
+    }
+    public void receiveWebOTP() {
+        createProgressBarProperties();
+        progress.setMessage("Sending OTP");
+        progress.show();
+        //test data
+        LoginDetails.getInstance().testData();
+        LoginDetails.getInstance().setIsverifying(true);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("userid",LoginDetails.getInstance().getUserid());
+        params.put("mobilenum",LoginDetails.getInstance().getMobilenum());
+        params.put("instruction", "0");
+        AsyncConnection as = new AsyncConnection(context,CommonResources.getURL("UserHandler"),"POST",params,false,null){
+            public void receiveData(JSONObject json){
+                try {
+                    String TAG_SUCCESS = "success";
+                    int success = json.getInt(TAG_SUCCESS);
+                    if (success == 0) {
+                        LoginDetails.getInstance().setOtp_received_from_web(json.getString("otp"));
+                        LoginDetails.getInstance().setIsverifying(true);
+                        CountDownTimer countDownTimer = new MyCountDownTimer(startTime, interval);
+                        countDownTimer.start();
+                        progress.setMessage("Waiting for OTP");
+                        //Add timer to progress bar
+
+                    }
+                    else if (success == 1) {
+
+                        LoginDetails.getInstance().setIsverifying(false);
+                        //Disable Progress bar
+                    }
+                    else {
+                        LoginDetails.getInstance().setIsverifying(false);
+                        //Disable Progress bar
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        as.execute();
+
+    }
+    private Boolean result = false;
+    ProgressDialog progress;
+    public Boolean getResult() {
+        return result;
+    }
+    public void setResult(Boolean result) {
+        this.result = result;
+    }
+
+    int i = 1;
+    public class MyCountDownTimer extends CountDownTimer {
+
+        public  MyCountDownTimer(long startTime,long interval)
+        {
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            if (!getResult())
+            {
+
+            }
+            progress.setMessage("Waiting for OTP \nResend OTP in "+( (startTime - i*interval)/1000)+" s");
+            i++;
+            verifyOTP();
+        }
+
+        @Override
+        public void onFinish() {
+            if (!getResult())
+            {
+                progress.setMessage("Couldn't Read OTP");
+                progress.dismiss();
+                setDialogFragment();
+//                btverify.setText("VERIFY");
+//                btverify.setActivated(true);
+            }
+
+
+        }
+    }
+    boolean isExecuted;
+    public void verifyOTP()
+    {
+        if (LoginDetails.getInstance().getIsverifying())
+        {
+            if( (LoginDetails.getInstance().getOtp_received_from_device()!=null) && (LoginDetails.getInstance().getOtp_received_from_web()!=null))
+            {
+
+                if(!isExecuted){
+                    isExecuted = true;
+                    setResult(true);
+                    //setProgress(50, "Verifying OTP");
+                    //btverify.setActivated(false);
+                    //btverify.setText("VERIFYING");
+                    doOTPVerification();
+
+                }
+
+            }
+        }
+        else if ( (LoginDetails.getInstance().getMob_verified()!=null) && (LoginDetails.getInstance().getMob_verified().equalsIgnoreCase("1")) )
+        {
+            progress.dismiss();
+            setResult(true);
+            //setProgress(100,"Verified");
+            //btverify.setActivated(false);
+            //btverify.setText("VERIFIED");
+        }
+
+    }
+    private final long startTime = 10 * 1000;
+    private final long interval = 1 * 1000;
+    public void doOTPVerification()
+    {
+        progress.setMessage("Verifying OTP");
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("userid",LoginDetails.getInstance().getUserid());
+        params.put("mobilenum",LoginDetails.getInstance().getMobilenum());
+        params.put("otp",LoginDetails.getInstance().getOtp_received_from_device());
+        params.put("instruction", "1");
+        AsyncConnection as = new AsyncConnection(context,CommonResources.getURL("UserHandler"),"POST",params,false,null){
+            public void receiveData(JSONObject json){
+                try {
+                    String TAG_SUCCESS = "success";
+                    int success = json.getInt(TAG_SUCCESS);
+                    if (success == 0) {
+                        LoginDetails.getInstance().setMob_verified(json.getString("is_verified"));
+                        LoginDetails.getInstance().setIsverifying(false);
+                        progress.dismiss();
+                        getFragmentManager().popBackStack();
+                    }
+                    else if (success == 1) {
+                        progress.dismiss();
+                        LoginDetails.getInstance().setIsverifying(false);// OTP Ver failed : Try Again Later
+                    }
+                    else {
+                        progress.dismiss();
+                        LoginDetails.getInstance().setIsverifying(false);// OTP Ver failed : Try Again Later
 
                     }
 
