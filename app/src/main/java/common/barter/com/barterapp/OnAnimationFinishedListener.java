a5689e15a1204efd68f3a96ce69ebbf237aa974e
pagalpanda <111.amit.p@gmail.com>
2016-05-15 10:07:03 +0530

    Refactored for MVP

---
+++ app/src/main/java/common/barter/com/barterapp/OnAnimationFinishedListener.java
@@ -0,0 +1,12 @@
+package common.barter.com.barterapp;
+
+/**
+ * Created by Panda on 15-05-2016.
+ */
+public interface OnAnimationFinishedListener {
+
+
+    void onSuccess();
+
+    void onFailure();
+}
